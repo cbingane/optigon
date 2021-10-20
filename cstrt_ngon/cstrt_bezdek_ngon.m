e6d6b0eb1ffe953ced8ffe039146ec99f00a0633
Christian Bingane <48734309+cbingane@users.noreply.github.com>
2021-10-21 00:00:13 +0100

    Add files via upload

---
+++ cstrt_ngon/cstrt_bezdek_ngon.m
@@ -0,0 +1,57 @@
+function [a,b] = cstrt_bezdek_ngon(n)
+if mod(log(n),log(2))==0 && n>=16
+    % initialization
+    syms u
+    v = 4*pi/n-3*u;
+    d = (2*cos(2*u+v)+1)/(2*cos(u)+cos(3*u+v));
+    eqn = 1+d^2-2*d*cos(u) == 2-2*cos(v);
+    u0 = double(vpasolve(eqn,u,[pi/n, 4*pi/(3*n)]));
+    v0 = 4*pi/n-3*u0;
+    d0 = (2*cos(2*u0+v0)+1)/(2*cos(u0)+cos(3*u0+v0));
+    r = ones(3*n/8,1);
+    for i = 1:3*n/16
+        r(2*i) = d0;
+    end
+    x = zeros(3*n/8,1);
+    for i = 1:3*n/8
+        if mod(i,3) == 2
+            x(i) = u0+v0;
+        else
+            x(i) = u0;
+        end
+    end
+    p = zeros(3*n/8-1,1); q = p;
+    p(1) = r(1)*sin(x(1)); q(1) = r(1)*cos(x(1));
+    for i = 2:3*n/8-1
+        p(i) = p(i-1) - (-1)^i*r(i)*sin(sum(x(1:i)));
+        q(i) = q(i-1) - (-1)^i*r(i)*cos(sum(x(1:i)));
+    end
+    % construction
+    a = zeros(n-1,1); b = a;
+    a(n/2-1) = p(1); a(n/2+1) = -a(n/2-1);
+    b(n/2-1) = q(1); b(n/2+1) = b(n/2-1);
+    a(n-1) = a(n/2-1)-sin(x(1)+v0); a(1) = -a(n-1);
+    b(n-1) = b(n/2-1)-cos(x(1)+v0); b(1) = b(n-1);
+    a(n-2) = p(2); a(2) = -a(n-2);
+    b(n-2) = q(2); b(2) = b(n-2);
+    for i = 1:n/8-1
+        a(mod(n/2-1+i*(3*n/2-2),n)) = p(3*i+1);
+        a(n-mod(n/2-1+i*(3*n/2-2),n)) = -a(mod(n/2-1+i*(3*n/2-2),n));
+        b(mod(n/2-1+i*(3*n/2-2),n)) = q(3*i+1);
+        b(n-mod(n/2-1+i*(3*n/2-2),n)) = b(mod(n/2-1+i*(3*n/2-2),n));
+        a(mod(n-1+i*(3*n/2-2),n)) = a(mod(n/2-1+i*(3*n/2-2),n))-(-1)^i*sin(sum(x(1:3*i+1))+u0);
+        a(n-mod(n-1+i*(3*n/2-2),n)) = -a(mod(n-1+i*(3*n/2-2),n));
+        b(mod(n-1+i*(3*n/2-2),n)) = b(mod(n/2-1+i*(3*n/2-2),n))-(-1)^i*cos(sum(x(1:3*i+1))+u0);
+        b(n-mod(n-1+i*(3*n/2-2),n)) = b(mod(n-1+i*(3*n/2-2),n));
+        a(mod(n-2+i*(3*n/2-2),n)) = p(3*i+2);
+        a(n-mod(n-2+i*(3*n/2-2),n)) = -a(mod(n-2+i*(3*n/2-2),n));
+        b(mod(n-2+i*(3*n/2-2),n)) = q(3*i+2);
+        b(n-mod(n-2+i*(3*n/2-2),n)) = b(mod(n-2+i*(3*n/2-2),n));
+        a(mod(n+i*(3*n/2-2),n)) = p(3*i);
+        a(n-mod(n+i*(3*n/2-2),n)) = -a(mod(n+i*(3*n/2-2),n));
+        b(mod(n+i*(3*n/2-2),n)) = q(3*i);
+        b(n-mod(n+i*(3*n/2-2),n)) = b(mod(n+i*(3*n/2-2),n));
+    end
+    a(n/2) = 0; b(n/2) = d0;
+end
+end
---
+++ cstrt_ngon/cstrt_fodor_ngon.m
@@ -0,0 +1,45 @@
+function [a,b] = cstrt_fodor_ngon(n)
+if mod(log(n),log(2))==0 && n>=8
+    % initialization
+    syms u
+    v = (pi/2-(n/4+1)*u)/(n/4-1);
+    d = (cos(v)-sin(u)+sin(u+v))/(cos(u)+sin(v));
+    eqn = 1+d^2-2*d*cos(u) == 2-2*cos(v);
+    u0 = double(vpasolve(eqn,u,[pi/n, 2*pi/(n+4)]));
+    v0 = (pi/2-(n/4+1)*u0)/(n/4-1);
+    d0 = (cos(v0)-sin(u0)+sin(u0+v0))/(cos(u0)+sin(v0));
+    r = ones(n/4+1,1);
+    for i = 1:n/8
+        r(2*i) = d0;
+    end
+    x = zeros(n/4+1,1);
+    x(1) = u0;
+    x(2:end-1) = u0+v0;
+    x(end) = u0;
+    p = zeros(n/4,1); q = p;
+    p(1) = r(1)*sin(x(1)); q(1) = r(1)*cos(x(1));
+    for i = 2:n/4
+        p(i) = p(i-1) - (-1)^i*r(i)*sin(sum(x(1:i)));
+        q(i) = q(i-1) - (-1)^i*r(i)*cos(sum(x(1:i)));
+    end
+    % construction
+    a = zeros(n-1,1); b = a;
+    a(n/2-1) = p(1); a(n/2+1) = -a(n/2-1);
+    b(n/2-1) = q(1); b(n/2+1) = b(n/2-1);
+    for i = 2:n/4-1
+        a(mod(i*(n/2-1),n)) = p(i);
+        a(n-mod(i*(n/2-1),n)) = -a(mod(i*(n/2-1),n));
+        b(mod(i*(n/2-1),n)) = q(i);
+        b(n-mod(i*(n/2-1),n)) = b(mod(i*(n/2-1),n));
+        a(mod((i-1)*(n/2-1)+n/2,n)) = a(mod((i-1)*(n/2-1),n)) - (-1)^i*sin(sum(x(1:i-1))+v0);
+        a(n-mod((i-1)*(n/2-1)+n/2,n)) = -a(mod((i-1)*(n/2-1)+n/2,n));
+        b(mod((i-1)*(n/2-1)+n/2,n)) = b(mod((i-1)*(n/2-1),n)) - (-1)^i*cos(sum(x(1:i-1))+v0);
+        b(n-mod((i-1)*(n/2-1)+n/2,n)) = b(mod((i-1)*(n/2-1)+n/2,n));
+    end
+    a(3*n/4) = p(n/4); a(n/4) = -a(3*n/4);
+    b(3*n/4) = q(n/4); b(n/4) = b(3*n/4);
+    a(3*n/4+1) = a(n/4+1) - sin(sum(x(1:n/4-1))+v0); a(n/4-1) = -a(3*n/4+1);
+    b(3*n/4+1) = b(n/4+1) - cos(sum(x(1:n/4-1))+v0); b(n/4-1) = b(3*n/4+1);
+    a(n/2) = 0; b(n/2) = d0;
+end
+end
